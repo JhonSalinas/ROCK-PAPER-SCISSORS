c38633b0c38dce1583eff9bc14e8a08d34f52e01
JhonSalinas <js.jhonsalinas@gmail.com>
2022-04-25 19:04:36 -0500

    added number of rounds functionality with game function

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
-//for computer taking
+//for computer election
+
 function computerPlay() {
     const items = ["rock", "paper", "scissors"];
     const item = items[Math.floor(Math.random()*3)];
     return item;
 }
+
 // for comparation
-const playerSelection = "rock".toLowerCase();
-let computerSelection = computerPlay();
 
 function playRound(playerSelection = playerSelection, computerSelection = computerSelection) {
     
     if(playerSelection=="rock" && computerSelection=="paper"){
         return "You Lose! Rock not beats Paper";
     }else if(playerSelection=="rock" && computerSelection=="scissors"){
+        ++winScore;
         return "You Win! Rock beats Scissors";
     }
     if(playerSelection=="paper" && computerSelection=="rock"){
@@ -26,6 +27,32 @@ function playRound(playerSelection = playerSelection, computerSelection = comput
     }else if(playerSelection=="scissors" && computerSelection=="rock"){
         return "You Lose! Scissors not beats Rock";
     }
+    //
+    if(playerSelection==computerSelection){
+        ++tiedScore;
+        return "tied Game";
+    }
 }
-//output
-console.log(playRound(playerSelection,computerSelection));
+
+//for score
+
+let winScore = 0;
+let tiedScore = 0;
+
+//game
+
+function game(){
+    let gameRounds = prompt("choose the game's rounds",1);
+    for(let i = 0; i < gameRounds; i++ ){
+        playerSelection = prompt("choose between: rock, paper or scissors").toLowerCase();
+        if(playerSelection == "paper" || playerSelection == "rock" || playerSelection == "scisors"){
+            alert(playRound(playerSelection, computerPlay()));
+        }else{
+            i--;
+            alert("your input not its valid, please only use: rock, paper or scissors");
+        }
+    }
+    alert(`your score is: ${winScore} wins, ${gameRounds - (winScore + tiedScore)} loses and ${tiedScore} tieds`);
+}
+
+game();
